924de003264ca3751983abc4818ee3cbb7317a97
happinessbaby <yueqipeng2021@gmail.com>
2023-01-06 20:34:15 -0600

    added fastapi features

--- assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
+++ assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
@@ -1,36 +1,62 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Query
 from pydantic import BaseModel
-from transformer import AutoModelForSeq2SeqLM
+from transformers import pipeline
+from typing import List
 
 
 
 #instantiate application
 app = FastAPI()
 
-pathToModel = "./model/translateToFrench"
-pipeline = AutoModelForSeq2SeqLM.from_pretrained(pathToModel)
+pathToModelFR = "./model/translateToFrench"
+pathToModelDE = "./model/translateToGerman"
+pipelineFR = pipeline(task = "translation_en_to_fr", model=pathToModelFR)
+pipelineDE = pipeline(task = "translation_en_to_de", model=pathToModelDE)
 
-class Body(BaseModel):
-    text: str
+class TextToTranslate(BaseModel):
+    input_text: str
+    
+class TextsToTranslate(BaseModel):
+    input_texts: List[str]
 
 #@: decorator that modifies this specific function
-# @app.get("/")
-# def index():
-#     return {"message": "Hello World"}
+@app.get("/")
+def index():
+    return {"message": "Hello World"}
 
-# @app.get("/cool")
-# def index1():
-#     return {"message": "this is cool"}
+@app.get("/cool")
+def index1():
+    return {"message": "this is cool"}
+
+@app.post("/echo") 
+def echo(message: str):
+    return {"message": message}
 
 
 @app.get('/')
 def root():
-    return Response("<h1>How to Serve AI Models with FastAPI and Docker</h1>")
+    return Response("<h1>AI Models with FastAPI</h1>")
 
-@app.post('/generate')
-def predict(body: Body):
-    results = pipeline(body.text)
-    return results[0]
+@app.post('/simpletranslate/{text_to_translate}')
+def predict(text_to_translate: str):
+    results = pipelineFR(text_to_translate)
+    return results
+
+@app.post('/translate/')
+def predict(text_to_translate: TextToTranslate):
+    results = pipelineFR(text_to_translate.input_text)
+    return results
+
+@app.post('/translateMultiple/')
+def predict(texts_to_translate: TextsToTranslate):
+    results = []
+    texts =  texts_to_translate.input_texts
+    print(texts)
+    for text in texts:
+        print(text);
+        results.append(pipelineDE(text))
+    return results
+
     
 if __name__ == "__main__":
     import uvicorn
